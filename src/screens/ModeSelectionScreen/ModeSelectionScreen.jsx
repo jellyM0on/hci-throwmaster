11ff344b7a0003d5fcf3e91ed864c088bd478c11
jellyM0on <ylana.ong@gmail.com>
2025-01-24 23:39:50 +0800

    feat: add initial mode selection screen

--- src/screens/ModeSelectionScreen/ModeSelectionScreen.jsx
+++ src/screens/ModeSelectionScreen/ModeSelectionScreen.jsx
@@ -1,13 +1,29 @@
 import "./ModeSelectionScreen.css"
+import bg from "../../assets/paper1.png"
 
 export default function ModeSelectionScreen({setScreen}) {
     return(
-        <div id="mode-selection">
-            <div>Choose a Mode</div>
-            <div>
-                <button onClick={() => setScreen("game_screen")}>Easy</button>
-                <button>Competitive</button>
-            </div>
-        </div>
+        <div id="tutorial-screen">
+                   <div id="tutorial-container">
+                       <img src={bg} alt="bg" id="tutorial-bg"/>
+                       <div id="tutorial-content">
+                           <h2>SELECT A GAME MODE</h2>
+                           <div>
+                                <button>Easy</button>
+                                <button>Difficult</button>
+                            </div>
+       
+       
+                           <div id="tutorial-next-btn">
+                            <button>test</button>
+                               {/* <button className="btn-plain" onClick={transitionToModeSelection}>Select Game Mode -&gt;</button> */}
+                           </div>
+                       </div>
+       
+                      
+                   </div>
+       
+                  
+               </div>
     )
 }
