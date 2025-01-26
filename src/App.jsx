83c1a5159799897e88ba88a6de87a02a14d5e2bf
jellyM0on <ylana.ong@gmail.com>
2025-01-27 03:11:53 +0800

    feat: add about skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import Home from "./pages/Home/Home";
 import GameCanvas from "./pages/GameCanvas/GameCanvas";
 import Layout from "./pages/Layout/Layout";
+import About from "./pages/About/About";
 
 import { useState } from "react";
 
@@ -16,6 +17,7 @@ function App() {
       <Route element={<Layout screen={screen} setScreen={setScreen}/>}>
           <Route path="/" element={<Home setScreen={setScreen}/>}/>
           <Route path="/game" element={<GameCanvas screen={screen} setScreen={setScreen}/>}/>
+          <Route path="/about" element={<About/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -24,12 +24,28 @@ export default function Home({setScreen}){
           }, 500); 
     }
 
+    const transitionToAbout = () => {
+        
+        const body = document.body;
+        body.style.overflow = "visible"; 
+
+        window.scrollTo({
+            top: 0,
+            left: 5000,
+            behavior: "smooth",
+        });
+       
+        setTimeout(() => {
+            navigate('/about');
+          }, 500); 
+    }
+
     return(
         <main id="home-container">
             <div id="home-title-container">
                 <h1>THROW <br></br> MASTER</h1>
                 <button className="button-1" onClick={transitionToGame}>Start</button>
-                <button className="button-2">About</button>
+                <button className="button-2" onClick={transitionToAbout}>About</button>
             </div>
             <div id="home-bg">
                 <img src={bg} alt="home background" />
