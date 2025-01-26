704cb65e83731f94a7e79559e31fea93f3d38a7f
jellyM0on <ylana.ong@gmail.com>
2025-01-27 06:49:50 +0800

    feat: update font for consistency

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,17 +5,17 @@ export default function About() {
         <div className="about-page">
             <div className="about-container">
                 <h1 className="about-title">About Us</h1>
-                <h1 className="about-description">
+                <p className="about-description">
                     Welcome to <strong>ThrowMaster</strong>! Our game is all about sorting waste into 
                     their appropriate bins to promote proper waste management and environmental 
                     awareness. This project is our creative take on making learning about 
                     sustainability fun and engaging.
-                </h1>
+                </p>
 
-                <h1 className="about-description">
+                <p className="about-description">
                     We are a group of students from the Polytechnic University of the Philippines, 
                     and this game serves as our final project for the Human Computer Interaction (HCI) course.
-                </h1>
+                </p>
 
                 <h1 className="about-team">
                     Meet our team:
@@ -30,10 +30,10 @@ export default function About() {
                     <li>Ylana Ong</li>
                 </ul>
 
-                <h1 className="about-description">
+                <p className="about-description">
                     Through <strong>ThrowMaster</strong>, we aim to inspire players to take small yet impactful steps 
                     towards a cleaner and greener future. Thank you for playing our game and supporting our mission!
-                </h1>
+                </p>
 
             </div>
         </div>
